506158b35c144ade5e78522e2c2920e77083476b
Fanny <55425039+FannyGautierr@users.noreply.github.com>
2023-05-04 12:59:20 +0200

    Feat/associate pokemons (#16)
    * create associations between users and pokemons
    
    * fix
    
    * add endpoints
    
    * add and delete pokemons
    
    * remove twig template
    
    ---------
    
    Co-authored-by: Pierre Grimaud <grimaud.pierre@gmail.com>

---
+++ src/Controller/PokemonController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\UserPokemon;
+use App\Helper\PokedexHelper;
+use App\Repository\PokemonRepository;
+use App\Repository\UserPokemonRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PokemonController extends AbstractController
+{
+    #[Route('/add', name: 'add_pokemon')]
+    public function add(
+        Request                $request,
+        EntityManagerInterface $entityManager,
+        PokemonRepository      $pokemonRepository,
+        UserPokemonRepository  $userPokemonRepository,
+    ): Response
+    {
+        $user = $this->getUser();
+
+        $data = $request->request->all();
+        $id = $data['id'];
+        $pokedex = $data['pokedex'];
+
+        $alreadyExist = $userPokemonRepository->findOneBy(['user' => $user, 'pokemon' => $id]);
+
+        if (!$alreadyExist) {
+            $pokemon = $pokemonRepository->findOneBy(['number' => $id]);
+
+            $userPokemon = new UserPokemon();
+            $userPokemon->setUser($user);
+            $userPokemon->setPokemon($pokemon);
+            $userPokemon->setShiny(false);
+            $userPokemon->setNormal(false);
+            $userPokemon->setLucky(false);
+            $userPokemon->setThreeStars(false);
+
+        } else {
+            $userPokemon = $alreadyExist;
+        }
+
+        if (!PokedexHelper::exist($pokedex)) {
+            return $this->json([
+                'message' => 'Pokedex not found',
+            ]);
+        }
+
+        $method = 'set' . ucfirst($pokedex);
+        $userPokemon->$method(true);
+
+        $entityManager->persist($userPokemon);
+        $entityManager->flush();
+
+        return $this->json([
+            'message' => 'Pokemon added',
+        ]);
+    }
+
+    #[Route('/delete', name: 'delete_pokemon')]
+    public function delete(
+        Request                $request,
+        EntityManagerInterface $entityManager,
+        UserPokemonRepository  $userPokemonRepository
+    ): Response
+    {
+        $data = $request->request->all();
+        $id = $data['id'];
+        $pokedex = $data['pokedex'];
+
+        $userPokemon = $userPokemonRepository->findOneBy(['user' => $this->getUser(), 'pokemon' => $id]);
+
+        if (!PokedexHelper::exist($pokedex)) {
+            return $this->json([
+                'message' => 'Pokedex not found',
+            ]);
+        }
+
+        if ($userPokemon) {
+            $method = 'set' . ucfirst($pokedex);
+            $userPokemon->$method(false);
+
+            $entityManager->persist($userPokemon);
+            $entityManager->flush();
+        }
+
+        return $this->json([
+            'message' => 'Pokemon deleted',
+        ]);
+    }
+}
