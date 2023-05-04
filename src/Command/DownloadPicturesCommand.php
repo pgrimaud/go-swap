183b36c7e9b584bb7e5127b7c5bf21d0f3084f2d
Fanny Gautier <fannygautier92@gmail.com>
2023-05-04 17:10:42 +0200

    download pokemons images

---
+++ src/Command/DownloadPicturesCommand.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Pokemon;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpClient\HttpClient;
+
+#[AsCommand(
+    name: 'app:download-pictures',
+    description: 'Download pictures for normal and shiny Pokemons'
+)]
+class DownloadPicturesCommand extends Command
+{
+    private SymfonyStyle $io;
+
+    public function __construct(private readonly EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io = new SymfonyStyle($input, $output);
+
+        $this->getPicturesShiny();
+        $this->getPicturesNormal();
+
+        $this->io->success('Done !');
+
+        return Command::SUCCESS;
+    }
+
+    private function getPicturesNormal(): void
+    {
+        $pokemonRepository = $this->entityManager->getRepository(Pokemon::class)->findAll();
+        $progressBar = new ProgressBar($this->io, count($pokemonRepository));
+        $progressBar->start();
+
+        foreach ($pokemonRepository as $pokemon) {
+            $progressBar->advance();
+
+            if (file_exists("public/images/normal/" . $pokemon->getNumber() . ".jpg")) {
+                continue;
+            }
+
+            $url = 'https://www.pokebip.com/pages/icones/poke/GO/' . $pokemon->getNumber() . '.png';
+            $image = file_get_contents($url);
+            $fp = fopen("public/images/normal/" . $pokemon->getNumber() . ".jpg", "w");
+            if ($fp && $image) {
+                fwrite($fp, $image);
+                fclose($fp);
+            }
+        }
+        $progressBar->finish();
+    }
+
+    private function getPicturesShiny(): void
+    {
+        $pokemonRepository = $this->entityManager->getRepository(Pokemon::class)->findBy(['isShiny' => true]);
+        $progressBar = new ProgressBar($this->io, count($pokemonRepository));
+        $progressBar->start();
+
+        foreach ($pokemonRepository as $pokemon) {
+            $progressBar->advance();
+
+            if (file_exists("public/images/shiny/" . $pokemon->getNumber() . ".jpg")) {
+                continue;
+            }
+
+            $endUrl = match ($pokemon->getNumber()) {
+                201 => 'f.png',
+                327 => 'p1.png',
+                710, 711 => 'fe.png',
+                default => '.png',
+            };
+            $url = 'https://www.pokebip.com/pages/icones/pokechroma/GO/' . $pokemon->getNumber() . $endUrl;
+
+            $image = file_get_contents($url);
+            $fp = fopen("public/images/shiny/" . $pokemon->getNumber() . ".jpg", "w");
+            if ($fp && $image) {
+                fwrite($fp, $image);
+                fclose($fp);
+            }
+        }
+        $progressBar->finish();
+    }
+}
