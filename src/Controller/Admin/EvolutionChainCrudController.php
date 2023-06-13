fc4a1868213b9a2e7a27728a687a349dff1d964c
Pierre Grimaud <grimaud.pierre@gmail.com>
2023-06-13 12:09:09 +0200

    Manage evolution chain

--- src/Controller/Admin/EvolutionChainCrudController.php
+++ src/Controller/Admin/EvolutionChainCrudController.php
@@ -7,6 +7,7 @@ use App\Entity\Pokemon;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class EvolutionChainCrudController extends AbstractCrudController
@@ -20,6 +21,7 @@ class EvolutionChainCrudController extends AbstractCrudController
     {
         return [
             TextField::new('name'),
+            NumberField::new('apiId')->hideOnIndex()->setHelp('Check API ID <a href="https://pokeapi.co/api/v2/evolution-chain?limit=1000">here</a>'),
             AssociationField::new('pokemons')
                 ->setFormTypeOption('choice_label', 'french_name')
                 ->setFormTypeOption('by_reference', false),
