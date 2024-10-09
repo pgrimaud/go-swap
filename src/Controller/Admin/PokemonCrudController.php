a451fc96a8cf173099e89c3dc1d744d11a467257
Pierre Grimaud <p.grimaud@sezane.com>
2024-10-09 19:08:39 +0200

    Add clone pokemon on admin

--- src/Controller/Admin/PokemonCrudController.php
+++ src/Controller/Admin/PokemonCrudController.php
@@ -4,7 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Pokemon;
 use App\Helper\GenerationHelper;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
@@ -12,12 +16,33 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 
 class PokemonCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private AdminUrlGenerator $adminUrlGenerator
+    )
+    {
+    }
+
     public static function getEntityFqcn(): string
     {
         return Pokemon::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $duplicate = Action::new('duplicate', 'Duplicate')
+            ->addCssClass('text-info')
+            ->linkToCrudAction('duplicate');
+
+        $actions->add(Crud::PAGE_INDEX, $duplicate);
+
+        return $actions;
     }
 
     public function configureCrud(Crud $crud): Crud
@@ -58,6 +83,36 @@ class PokemonCrudController extends AbstractCrudController
                 ->setHelp('Check shiny pictures <a target="_blank" href="https://www.pokekalos.fr/pokedex/pokemongo/index.html">here</a>'),
             AssociationField::new('evolutionChain')->autocomplete()->hideOnIndex()->setRequired(false),
         ];
+    }
+
+    public function duplicate(AdminContext $adminContext): Response
+    {
+        /** @var Pokemon $pokemon */
+        $pokemon = $adminContext->getEntity()->getInstance();
+        $newPokemon = clone $pokemon;
+
+        try {
+            $this->entityManager->persist($newPokemon);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', sprintf('%s was duplicated', $pokemon->getFrenchName()));
+
+            $redirectUrl = $this->adminUrlGenerator
+                ->setController(PokemonCrudController::class)
+                ->setAction(Action::DETAIL)
+                ->setEntityId($newPokemon->getId())
+                ->generateUrl();
+        } catch (\Throwable $exception) {
+            $this->addFlash('danger', $exception->getMessage());
+
+            $redirectUrl = $this->adminUrlGenerator
+                ->setController(PokemonCrudController::class)
+                ->setAction(Action::DETAIL)
+                ->setEntityId($pokemon->getId())
+                ->generateUrl();
+        }
+
+        return $this->redirect($redirectUrl);
     }
 
 }
