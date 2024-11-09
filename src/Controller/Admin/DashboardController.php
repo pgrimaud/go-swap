9a428ccf89d3342029c798ce37cb023a4462aae1
Pierre Grimaud <grimaud.pierre@gmail.com>
2024-11-09 14:25:40 +0100

    add shortlink to download picture in admin dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,13 +8,21 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DashboardController extends AbstractDashboardController
 {
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        private KernelInterface $kernel
+    )
     {
     }
 
@@ -32,6 +40,22 @@ class DashboardController extends AbstractDashboardController
             ->setTitle('Go Swap');
     }
 
+    #[Route('/admin/update-pictures', name: 'admin_update_pictures')]
+    public function updatePictures(): Response
+    {
+        $application = new Application($this->kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput(['command' => 'app:download-pictures']);
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+        
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        /* @phpstan-ignore-next-line */
+        return $this->redirect($adminUrlGenerator->setController(PokemonCrudController::class)->generateUrl());
+    }
+
     public function configureMenuItems(): iterable
     {
         $websiteUrl = $this->urlGenerator->generate('app_index');
@@ -39,5 +63,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::section('Admin');
         yield MenuItem::linkToCrud('Pokemons', 'fas fa-list', Pokemon::class);
         yield MenuItem::linkToCrud('Evolution chains', 'fas fa-link', EvolutionChain::class);
+        yield MenuItem::section('Commands');
+        yield MenuItem::linkToUrl('Update pictures', 'fa fa-image', $this->urlGenerator->generate('admin_update_pictures'));
     }
 }
