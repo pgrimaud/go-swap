e161de62d8b1264884f6c03da903bfebd5fa1077
Pierre Grimaud <grimaud.pierre@gmail.com>
2025-04-25 21:39:25 +0200

    feat: init project

--- migrations/Version20250425193243.php
+++ migrations/Version20250425193243.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230502141645 extends AbstractMigration
+final class Version20250425193243 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,16 @@ final class Version20230502141645 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE pokemon (id INT AUTO_INCREMENT NOT NULL, number INT NOT NULL, generation VARCHAR(255) NOT NULL, french_name VARCHAR(255) NOT NULL, english_name VARCHAR(255) DEFAULT NULL, is_shiny TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql(<<<'SQL'
+            CREATE TABLE type (id INT AUTO_INCREMENT NOT NULL, nale VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, icon VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE pokemon');
+        $this->addSql(<<<'SQL'
+            DROP TABLE type
+        SQL);
     }
 }
--- src/Entity/Type.php
+++ src/Entity/Type.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\TypeRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TypeRepository::class)]
@@ -13,10 +11,10 @@ class Type
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    private ?int $id = null;
+    private int $id;
 
     #[ORM\Column(length: 255)]
-    private ?string $name = null;
+    private ?string $nale = null;
 
     #[ORM\Column(length: 255)]
     private ?string $slug = null;
@@ -24,44 +22,19 @@ class Type
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $icon = null;
 
-    /**
-     * @var Collection<int, Pokemon>
-     */
-    #[ORM\ManyToMany(targetEntity: Pokemon::class, mappedBy: 'types')]
-    private Collection $pokemons;
-
-    /**
-     * @var Collection<int, TypeEffectiveness>
-     */
-    #[ORM\OneToMany(mappedBy: 'sourceType', targetEntity: TypeEffectiveness::class, orphanRemoval: true)]
-    private Collection $effectivenesses;
-
-    /**
-     * @var Collection<int, Move>
-     */
-    #[ORM\OneToMany(mappedBy: 'Type', targetEntity: Move::class, orphanRemoval: true)]
-    private Collection $moves;
-
-    public function __construct()
-    {
-        $this->pokemons = new ArrayCollection();
-        $this->effectivenesses = new ArrayCollection();
-        $this->moves = new ArrayCollection();
-    }
-
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getNale(): ?string
     {
-        return $this->name;
+        return $this->nale;
     }
 
-    public function setName(string $name): static
+    public function setNale(string $nale): static
     {
-        $this->name = $name;
+        $this->nale = $nale;
 
         return $this;
     }
@@ -86,112 +59,6 @@ class Type
     public function setIcon(?string $icon): static
     {
         $this->icon = $icon;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Pokemon>
-     */
-    public function getPokemons(): Collection
-    {
-        return $this->pokemons;
-    }
-
-    public function addPokemon(Pokemon $pokemon): static
-    {
-        if (!$this->pokemons->contains($pokemon)) {
-            $this->pokemons->add($pokemon);
-            $pokemon->addType($this);
-        }
-
-        return $this;
-    }
-
-    public function removePokemon(Pokemon $pokemon): static
-    {
-        if ($this->pokemons->removeElement($pokemon)) {
-            $pokemon->removeType($this);
-        }
-
-        return $this;
-    }
-
-    public function __toString(): string
-    {
-        return (string) $this->name;
-    }
-
-    /**
-     * @return Collection<int, TypeEffectiveness>
-     */
-    public function getEffectivenesses(): Collection
-    {
-        return $this->effectivenesses;
-    }
-
-    public function addEffectiveness(TypeEffectiveness $effectiveness): static
-    {
-        if (!$this->effectivenesses->contains($effectiveness)) {
-            $this->effectivenesses->add($effectiveness);
-            $effectiveness->setSourceType($this);
-        }
-
-        return $this;
-    }
-
-    public function removeEffectiveness(TypeEffectiveness $effectiveness): static
-    {
-        if ($this->effectivenesses->removeElement($effectiveness)) {
-            // set the owning side to null (unless already changed)
-            if ($effectiveness->getSourceType() === $this) {
-                $effectiveness->setSourceType(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getStrongAgainst(): Collection
-    {
-        return $this->effectivenesses->filter(function (TypeEffectiveness $effectiveness) {
-            return $effectiveness->getMultiplier() > 1;
-        });
-    }
-
-    public function getBestDefender(): Collection
-    {
-        return $this->effectivenesses->filter(function (TypeEffectiveness $effectiveness) {
-            return $effectiveness->getMultiplier() < 1;
-        });
-    }
-
-    /**
-     * @return Collection<int, Move>
-     */
-    public function getMoves(): Collection
-    {
-        return $this->moves;
-    }
-
-    public function addMove(Move $move): static
-    {
-        if (!$this->moves->contains($move)) {
-            $this->moves->add($move);
-            $move->setType($this);
-        }
-
-        return $this;
-    }
-
-    public function removeMove(Move $move): static
-    {
-        if ($this->moves->removeElement($move)) {
-            // set the owning side to null (unless already changed)
-            if ($move->getType() === $this) {
-                $move->setType(null);
-            }
-        }
 
         return $this;
     }
